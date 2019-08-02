656812951717820eada8372b53690eb83e0a593d
L.J. Hill <foehawk@gmail.com>
2019-08-01 21:44:55 -0400

    Add active channel info to "info" command output

--- rokuctl/actions/info.py
+++ rokuctl/actions/info.py
@@ -22,5 +22,19 @@ class Info(Action):
             return
 
         tree = ElementTree.fromstring(resp.text)
-        fields_and_values = ((el.tag, el.text) for el in tree)
+        fields_and_values = [(el.tag, el.text) for el in tree]
         print('\n'.join('{}: {}'.format(field, value) for field, value in fields_and_values))
+
+        is_tv = any(str(v).lower() == 'true' for k, v in fields_and_values if k.lower() == 'is-tv')
+        if not is_tv:
+            return
+
+        try:
+            resp = requests.get('http://{}:8060/query/tv-active-channel'.format(args.ip.strip()))
+        except requests.exceptions.ConnectionError as ex:
+            print(str(ex), file=sys.stderr)
+            return
+
+        tree = ElementTree.fromstring(resp.text)
+        fields_and_values = ((el.tag, el.text) for el in tree.find('channel'))
+        print('\n'.join('channel-{}: {}'.format(field, value) for field, value in fields_and_values))
